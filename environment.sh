c90f7c58ac7ff367cac664e8539105eb93725515
uncleF <uncle.funkay@gmail.com>
2015-10-30 00:45:10 +0300

    Project Update

--- environment.sh
+++ environment.sh
@@ -1,17 +1,22 @@
 #Disable Press-and-hold for Keys in Favor of Key Repeat
+
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 #Open Everything in Finder's Columns View
+
 defaults write com.apple.Finder FXPreferredViewStyle clmv
 
 #Fast Key Repeat
+
 defaults write NSGlobalDomain KeyRepeat -int 0
 
 #Show Different Volumes on the Desktop
+
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
 #Setup Safari for Development
+
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
@@ -19,10 +24,13 @@ defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.Web
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 #Copy Command Line Tools
+
 cp ./{.aliases,.environment,.functions,.prompt} ~/
 
 #Load Command Line Tools in the .bashrc
+
 echo -e "\n\n[[ -s ~/.environment ]] && source ~/.environment\n[[ -s ~/.aliases ]] && source ~/.aliases\n[[ -s ~/.functions ]] && source ~/.functions\n[[ -s ~/.prompt ]] && source ~/.prompt" >> ~/.bashrc
 
 #Copy Development Tools
+
 cp ./{*.properties,*.json} ../

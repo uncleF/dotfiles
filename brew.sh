d179fdef7ecdde48175824745a01a3994e7d5686
Ilya Biryukov <uncle.funkay@gmail.com>
2017-02-10 12:50:02 +0300

    Project Update

--- brew.sh
+++ brew.sh
@@ -5,3 +5,40 @@ brew upgrade
 
 # ios-webkit-debug-proxy
 # wget
+
+# brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
+
+# # daily
+# brew cask install spectacle
+# brew cask install dropbox
+# brew cask install gyazo
+# brew cask install 1password
+# brew cask install rescuetime
+# brew cask install flux
+
+# # dev
+# brew cask install iterm2
+# brew cask install sublime-text
+# brew cask install imagealpha
+# brew cask install imageoptim
+
+# # fun
+# brew cask install limechat
+# brew cask install miro-video-converter
+# brew cask install horndis               # usb tethering
+
+# # browsers
+# brew cask install google-chrome-canary
+# brew cask install firefoxnightly
+# brew cask install webkit-nightly
+# brew cask install chromium
+# brew cask install torbrowser
+
+# # less often
+# brew cask install disk-inventory-x
+# brew cask install screenflow
+# brew cask install vlc
+# brew cask install gpgtools
+# brew cask install licecap
+# brew cask install utorrent

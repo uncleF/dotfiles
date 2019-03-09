1960a779e65c7229a063b6aacb331fe3aa36f5f3
uncle Fucka <unclefucka@MacBook-Pro-ila.local>
2019-03-09 15:44:48 +0300

    update brew.sh

--- brew.sh
+++ brew.sh
@@ -5,17 +5,18 @@ brew upgrade
 
 # Homebrew Packages
 
-brew install ffmpeg --with-libvpx
-brew install grep --with-default-names
+brew install ffmpeg
+brew install grep
 brew install guetzli
-brew install imagemagick --with-webp
+brew install imagemagick
 brew install ios-webkit-debug-proxy
 brew install less
 brew install node
+brew install nvm
 brew install rename
 brew install trash
 brew install tree
-brew install wget --with-iri
+brew install wget
 brew install zopfli
 
 # Cask
@@ -26,19 +27,42 @@ brew tap caskroom/versions
 # Apps
 
 brew cask install 1password
+brew cask install anki
+brew cask install appcleaner
+brew cask install aseprite
+brew cask install bartender
+brew cask install bitwarden
+brew cask install blender
+brew cask install ccleaner
+brew cask install cyberduck
+brew cask install docker
 brew cask install dropbox
+brew cask install evernote
+brew cask install figma
 brew cask install flux
+brew cask install xquartz
+brew cask install fontforge
 brew cask install horndis
 brew cask install imagealpha
 brew cask install imageoptim
 brew cask install iterm2
 brew cask install licecap
+brew cask install punto-switcher
 brew cask install screenflow
+brew cask install sketch
+brew cask install skype
 brew cask install slack
+brew cask install signal
+brew cask install skitch
 brew cask install spectacle
+brew cask install steam
 brew cask install sublime-text
+brew cask install teamviewer
+brew cask install telegram
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install visual-studio-code
 brew cask install vlc
-brew cask install torbrowser
 
 # Browsers
 
@@ -46,11 +70,14 @@ brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install chromium
 brew cask install firefox
-brew cask install firefoxnightly
-brew cask install firefoxdeveloperedition
+brew cask install firefox-nightly
+brew cask install firefox-developer-edition
 brew cask install opera
 brew cask install opera-beta
 brew cask install opera-developer
-brew cask install webkit-nightly
+brew cask install tor-browser
+brew cask install brave-browser
+
+# Cleanup
 
 brew cleanup
